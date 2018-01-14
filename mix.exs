772883729ec02bd512c531ea0a69c3b4abdb5dfc
Serge Tyatin <700@2rba.com>
2018-01-14 20:27:09 +0200

    Add Timex lib

--- mix.exs
+++ mix.exs
@@ -41,7 +41,9 @@ defmodule Cp.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:httpoison, "~> 0.13"}
+      {:httpoison, "~> 0.13"},
+      {:timex, "~> 3.1"},
+      {:timex_ecto, "~> 3.0"}
     ]
   end
 
